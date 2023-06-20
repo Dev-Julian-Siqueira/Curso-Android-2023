62a6b35bcbe74aba0f5b4af059272062ff07e90e
Dev-Julian-Siqueira <juliansiqueiradev@outlook.com>
2023-06-20 16:09:21 -0300

    Projeto Aplicativo GasEta.

---
+++ AppGasEta/app/src/main/java/devandroid/julian/appgaseta/view/GasEtaActivity.java
@@ -0,0 +1,118 @@
+package devandroid.julian.appgaseta.view;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import devandroid.julian.appgaseta.R;
+import devandroid.julian.appgaseta.apoio.UtilGasEta;
+
+public class GasEtaActivity extends AppCompatActivity {
+
+    EditText editGasolina;
+    EditText editEtanol;
+
+    TextView txtResultado;
+
+    Button btnCalcular;
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
+
+    double precoGasolina;
+    double precoEtanol;
+    String recomendacao;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_gaseta);
+
+        editGasolina = findViewById(R.id.editGasolina);
+        editEtanol = findViewById(R.id.editEtanol);
+
+        txtResultado = findViewById(R.id.txtResultado);
+
+        btnCalcular = findViewById(R.id.btnCalcular);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                boolean isDadosOk = true;
+
+                if(TextUtils.isEmpty(editGasolina.getText())){
+                    editGasolina.setError("* Obrigatório");
+                    editGasolina.requestFocus();
+                    isDadosOk = false;
+                }
+
+                if(TextUtils.isEmpty(editEtanol.getText())){
+                    editEtanol.setError("* Obrigatório");
+                    editEtanol.requestFocus();
+                    isDadosOk = false;
+                }
+
+                if(isDadosOk){
+
+                    precoGasolina = Double.parseDouble(editGasolina.getText().toString());
+                    precoEtanol = Double.parseDouble(editEtanol.getText().toString());
+
+                    recomendacao = UtilGasEta.calcularMelhorOpcao(precoGasolina,precoEtanol);
+
+                    txtResultado.setText(recomendacao);
+
+
+                }else{
+                    Toast.makeText(GasEtaActivity.this,
+                            "Por favor, digite os dados obrigatórios...",
+                            Toast.LENGTH_LONG).show();
+                }
+
+
+
+
+
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                editEtanol.setText("");
+                editGasolina.setText("");
+                txtResultado.setText("RESULTADO");
+
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GasEtaActivity.this, "GasEta Finalizado...",
+                        Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+    }
+}
